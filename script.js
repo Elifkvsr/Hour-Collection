d9576a5eaca3aaeb9bddbddf2816b3f223293395
Elif Kevser Tölek <152923266+Elifkvsr@users.noreply.github.com>
2024-11-09 08:27:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+const bxle = document.getElementsByClassName("bx")[0];
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+
+left.addEventListener('click', () => {
+    bxle.scrollLeft += 100;
+});
+
+right.addEventListener('click', () => {
+    bxle.scrollLeft -= 100;
+});
+
+
+
+const scroll_watch = document.getElementsByClassName("scroll_watch");
+const main_watch = document.getElementsByClassName("main_watch")[0];
+const img = document.getElementsByClassName("img")[0];
+const svg = document.getElementById("svg");
+const span_number = document.getElementById("span_number");
+const h6 = document.getElementsByTagName("h6")[0];
+
+scroll_watch[0].addEventListener('click', () => {
+    main_watch.src = "img/watch.png";
+    svg.innerHTML = `<text x="0%" y="90%">Gen 5</text>`
+    h6.innerHTML = "$359.00";
+    span_number.innerText = 1;
+    setTimeout(() => {
+        img.style.bottom = "-100px";
+    }, 500);
+    setTimeout(() => {
+        img.style.bottom = "-230px";
+    }, 1000);
+    setTimeout(() => {
+        img.src = "img/img3.png";
+    }, 1900);
+    setTimeout(() => {
+        img.style.bottom = "0px";
+    }, 2300);
+});
+
+
+scroll_watch[1].addEventListener('click', () => {
+    main_watch.src = "img/watch2.png";
+    svg.innerHTML = `<text x="0%" y="90%">Gen 7</text>`
+    h6.innerHTML = "$450.00";
+    span_number.innerText = 2;
+    setTimeout(() => {
+        img.style.bottom = "-100px";
+    }, 500);
+    setTimeout(() => {
+        img.style.bottom = "-230px";
+    }, 1000);
+    setTimeout(() => {
+        img.src = "img/img2.png";
+    }, 1900);
+    setTimeout(() => {
+        img.style.bottom = "0px";
+    }, 2300);
+});
+
+scroll_watch[2].addEventListener('click', () => {
+    main_watch.src = "img/watch3.png";
+    svg.innerHTML = `<text x="0%" y="90%">Gen 9</text>`
+    h6.innerHTML = "$480.00";
+    span_number.innerText = 3;
+    setTimeout(() => {
+        img.style.bottom = "-100px";
+    }, 500);
+    setTimeout(() => {
+        img.style.bottom = "-230px";
+    }, 1000);
+    setTimeout(() => {
+        img.src = "img/img3.png";
+    }, 1900);
+    setTimeout(() => {
+        img.style.bottom = "0px";
+    }, 2300);
+});
+
+
+
+const cart = document.getElementById("cart");
+const add_to_cart = document.getElementsByClassName("add_to_cart")[0];
+a = 0;
+
+cart.innerText = a;
+
+add_to_cart.addEventListener("click", () => {
+    a += 1;
+    cart.innerText = a;
+})
